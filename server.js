89b9b77645d571513a2c810adc2ea844ecd78bcc
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-02-15 19:21:10 -0600

    General update

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const db = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
 
-// app.get('/notes', (req, res) =>
-//   res.sendFile(path.join(__dirname, './public/notes.html'))
-// );
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
-// app.get('/routes', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/routes.html'))
-// );
+app.get('/api/notes', (req, res) => 
+  res.json(db)
+  
+);
+
+app.post('/api/notes', (req, res) => 
+  res.json(db)
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
